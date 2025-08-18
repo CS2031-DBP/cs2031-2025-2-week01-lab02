3938c96a23c9601a627e38b80648d91058b5206d
AlejandroEN <alejandroescobarnunez@gmail.com>
2025-08-18 03:54:26 -0500

    Initial Commit

---
+++ src/main/java/com/cs203120252/week01lab02/Cs203120252Week01Lab02Application.java
@@ -0,0 +1,11 @@
+package com.cs203120252.week01lab02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs203120252Week01Lab02Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Cs203120252Week01Lab02Application.class, args);
+    }
+}
---
+++ src/test/java/com/cs203120252/week01lab02/Cs203120252Week01Lab02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs203120252.week01lab02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs203120252Week01Lab02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
