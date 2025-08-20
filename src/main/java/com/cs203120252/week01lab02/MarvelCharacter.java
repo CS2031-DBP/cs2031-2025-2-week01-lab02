f93c58345408a8958efc6ac4da30c7a5e0893e40
jdluques <juan.luque@utec.edu.pe>
2025-08-20 17:13:18 -0500

    refactor: replace explicit getters, setters, and constructors with Lombok annotations

--- src/main/java/com/cs203120252/week01lab02/MarvelCharacter.java
+++ src/main/java/com/cs203120252/week01lab02/MarvelCharacter.java
@@ -1,5 +1,14 @@
 package com.cs203120252.week01lab02;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class MarvelCharacter {
     private Long id;
     private String name;
@@ -8,73 +17,4 @@ public class MarvelCharacter {
     private String firstAppearance;
     private String powers;
     private String origin;
-
-    public MarvelCharacter() {
-    }
-
-    public MarvelCharacter(Long id, String name, String realName, String team, String firstAppearance, String powers, String origin) {
-        this.id = id;
-        this.name = name;
-        this.realName = realName;
-        this.team = team;
-        this.firstAppearance = firstAppearance;
-        this.powers = powers;
-        this.origin = origin;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRealName() {
-        return realName;
-    }
-
-    public void setRealName(String realName) {
-        this.realName = realName;
-    }
-
-    public String getTeam() {
-        return team;
-    }
-
-    public void setTeam(String team) {
-        this.team = team;
-    }
-
-    public String getFirstAppearance() {
-        return firstAppearance;
-    }
-
-    public void setFirstAppearance(String firstAppearance) {
-        this.firstAppearance = firstAppearance;
-    }
-
-    public String getPowers() {
-        return powers;
-    }
-
-    public void setPowers(String powers) {
-        this.powers = powers;
-    }
-
-    public String getOrigin() {
-        return origin;
-    }
-
-    public void setOrigin(String origin) {
-        this.origin = origin;
-    }
 }
